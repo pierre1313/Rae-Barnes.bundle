82e3e2287a5fa0ab58e88dc23010b17bb6a2b2db
Pierre <p.dellanave@gmail.com>
2011-03-30 01:36:47 -0700

    Converted to v2

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,40 +1,52 @@
-from PMS import Plugin, Log, XML, HTTP, JSON, RSS, Utils
-from PMS.MediaXML import MediaContainer, DirectoryItem, PhotoItem
-
 PLUGIN_PREFIX   = "/photos/RaeBarnes"
 ROOT_URL        = "http://www.raebarnes.com"
 RSS_FEED        = ROOT_URL + "/rss/"
 
 ####################################################################################################
 def Start():
-  Plugin.AddRequestHandler(PLUGIN_PREFIX, HandlePhotosRequest, "Rae Barnes", "icon-default.png", "art-default.png")
-  Plugin.AddViewGroup("Pictures", viewMode="Pictures", contentType="photos")
-  Plugin.AddViewGroup("Details", viewMode="InfoList", contentType="items")
+
+  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "Rae Barnes", "icon-default.png", "art-default.png")
+  Plugin.AddViewGroup("Pictures", viewMode="Pictures", mediaType="photos")
+  Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
+  MediaContainer.art = "art-default.png"
+  DirectoryItem.thumb = "icon-default.png"
 
 ####################################################################################################
-def HandlePhotosRequest(pathNouns, count):
-  if count == 0:
-    dir = MediaContainer("art-default.png", "Details", "Rae Barnes")
-    for item in RSS.Parse(RSS_FEED).entries:
-      entry = XML.ElementFromString(item.description, True)
-      imgs = entry.xpath('//img')
-      if len(imgs) > 5:
-        thumb = ''
-        for img in imgs:
-          if img.get('src').find('raebarnes.com/images/content') != -1:
-            thumb = img.get('src')
-            
-        summary = entry.text_content()[:400] + '...'
-        dir.AppendItem(DirectoryItem(Utils.EncodeStringToUrlPath(item.link)+'$'+Utils.EncodeStringToUrlPath(item.title), item.title, thumb, summary))
+def MainMenu():
+    dir = MediaContainer(title2="Details", title1="Rae Barnes")
+    for item in XML.ElementFromURL(RSS_FEED).xpath("//item"):
+      #Log(HTML.StringFromElement(item))
       
-  elif count == 1:
-    (url,title) = pathNouns[0].split('$')
-    dir = MediaContainer("art-default.png", "Pictures", "Rae Barnes", Utils.DecodeUrlPathToString(title))
-    count = 1
-    for img in XML.ElementFromURL(Utils.DecodeUrlPathToString(url), True).xpath('//img'):
-      if img.get('src').find('/images/content') != -1:
-        url = ROOT_URL + img.get('src')
-        dir.AppendItem(PhotoItem(url, 'Photo %d' % count, '', url))
-        count += 1
+      title = item.xpath('.//title')[0].text
+      url = item.xpath('.//guid')[0].text
+      raw_summary = HTML.StringFromElement(item.xpath('.//description')[0]).replace(']]','').replace('<![CDATA[','').replace('&gt;','>').replace('&lt;','<').replace('&nbsp;','')#.text
+      summary = HTML.ElementFromString(raw_summary).text_content()[:400]+"..."
+      try:
+        thumb = HTML.ElementFromString(raw_summary).xpath('.//img')[0].get('src')
+      except:
+        thumb = None
+        
+      dir.Append(Function(DirectoryItem(PictureMenu, title=title, thumb=thumb, summary=summary), url=url))
+     
+     #  entry = HTML.ElementFromString(item.description)
+#       imgs = entry.xpath('//img')
+#       if len(imgs) > 5:
+#         thumb = ''
+#         for img in imgs:
+#           if img.get('src').find('raebarnes.com/images/content') != -1:
+#             thumb = img.get('src')
+#             
+#         summary = entry.text_content()[:400] + '...'
+#         dir.Append(Function(DirectoryItem(PictureMenu, item.title, thumb, summary),url=item.link,title=item.title))
+    return dir
+
+def PictureMenu(sender, url,title = ''):
+  dir = MediaContainer(viewGroup="Pictures", title1="Rae Barnes", title2=title)
+  count = 1
+  for img in HTML.ElementFromURL(url).xpath('//img'):
+    if img.get('src').find('/images/content') != -1:
+      url = ROOT_URL + img.get('src')
+      dir.Append(PhotoItem(url, 'Photo %d' % count, '', url))
+      count += 1
     
-  return dir.ToXML()
+  return dir
