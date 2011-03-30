2470cf019ae89048112a0f38eb3eda300885d23f
Pierre <p.dellanave@gmail.com>
2011-03-30 01:52:47 -0700

    Code cleanup - comments removal

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -5,18 +5,16 @@ RSS_FEED        = ROOT_URL + "/rss/"
 ####################################################################################################
 def Start():
 
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "Rae Barnes", "icon-default.png", "art-default.png")
+  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "Rae Barnes", "icon-default.png", "art-default.jpg")
   Plugin.AddViewGroup("Pictures", viewMode="Pictures", mediaType="photos")
   Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
-  MediaContainer.art = "art-default.png"
+  MediaContainer.art = "art-default.jpg"
   DirectoryItem.thumb = "icon-default.png"
 
 ####################################################################################################
 def MainMenu():
     dir = MediaContainer(title2="Details", title1="Rae Barnes")
     for item in XML.ElementFromURL(RSS_FEED).xpath("//item"):
-      #Log(HTML.StringFromElement(item))
-      
       title = item.xpath('.//title')[0].text
       url = item.xpath('.//guid')[0].text
       raw_summary = HTML.StringFromElement(item.xpath('.//description')[0]).replace(']]','').replace('<![CDATA[','').replace('&gt;','>').replace('&lt;','<').replace('&nbsp;','')#.text
@@ -28,16 +26,6 @@ def MainMenu():
         
       dir.Append(Function(DirectoryItem(PictureMenu, title=title, thumb=thumb, summary=summary), url=url))
      
-     #  entry = HTML.ElementFromString(item.description)
-#       imgs = entry.xpath('//img')
-#       if len(imgs) > 5:
-#         thumb = ''
-#         for img in imgs:
-#           if img.get('src').find('raebarnes.com/images/content') != -1:
-#             thumb = img.get('src')
-#             
-#         summary = entry.text_content()[:400] + '...'
-#         dir.Append(Function(DirectoryItem(PictureMenu, item.title, thumb, summary),url=item.link,title=item.title))
     return dir
 
 def PictureMenu(sender, url,title = ''):
